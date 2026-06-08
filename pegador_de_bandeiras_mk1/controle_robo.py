61119fb77667d49b1160bf7ef05e02759903b95f
Joao Lopes <jp.lopes.melo01@gmail.com>
2026-06-07 23:23:32 -0300

    URDF modificado do Caio e lógica de desvio aprimorada

--- pegador_de_bandeiras_mk1/controle_robo.py
+++ pegador_de_bandeiras_mk1/controle_robo.py
@@ -5,7 +5,7 @@ from rclpy.node import Node
 from sensor_msgs.msg import LaserScan, Imu, Image
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist
-
+from std_msgs.msg import Float64MultiArray
 from scipy.spatial.transform import Rotation as R
 
 from cv_bridge import CvBridge
@@ -22,6 +22,10 @@ class Estados(Enum):
     POSICIONANDO_PARA_COLETA = 4
     CAPTURANDO_BANDEIRA = 5
 
+class DirecoesRotacao(Enum):
+    ESQUERDA = 1
+    DIREITA = -1
+
 
 class ControleRobo(Node):
 
@@ -30,7 +34,8 @@ class ControleRobo(Node):
 
         # Publisher para comando de velocidade
         self.cmd_vel_pub = self.create_publisher(Twist, '/cmd_vel', 10)
-
+        # Publisher para controle da garra
+        self.gripper_pub = self.create_publisher(Float64MultiArray, '/gripper_controller/commands', 10)
         # Subscribers
         self.create_subscription(LaserScan, '/scan', self.scan_callback, 10)
         self.create_subscription(Imu, '/imu', self.imu_callback, 10)
@@ -54,9 +59,13 @@ class ControleRobo(Node):
         self.porcentagem_bandeira_na_camera = 0
         self.pos_x_bandeira_camera = -1
         self.centro_x_camera = -1
-        self.distancias = None
         self.tempo_desviando = -1
         self.direcao_desvio = -1
+
+        # DETECÇÃO DE OBSTACULOS À FRENTE - Índices de -35° a +35°
+        self.indices_frente_esquerda = list(range(0, 35))
+        self.indices_frente_direita = list(range(325, 360))
+
 
         self.estado_atual = Estados.EXPLORANDO
         self.estado_anterior = None
@@ -67,57 +76,55 @@ class ControleRobo(Node):
         if num_ranges == 0:
             return
         
-        distancia_max_obstaculo_frente = 0.64
+        distancia_max_obstaculo_frente = 0.62
         distancia_max_obstaculo_lados = 0.40
 
         # DETECÇÃO DE OBSTACULOS À FRENTE - Índices de -35° a +35°
-        indices_frente_esquerda = list(range(0, 35))
-        self.distancias = [msg.ranges[i] for i in indices_frente_esquerda]
-        self.obstaculo_a_frente_esquerda = self.distancias and min(self.distancias) < distancia_max_obstaculo_frente
+        distancias = [msg.ranges[i] for i in self.indices_frente_esquerda]
+        self.obstaculo_a_frente_esquerda = distancias and min(distancias) < distancia_max_obstaculo_frente
 
-        indices_frente_direita = list(range(325, 360))
-        self.distancias = [msg.ranges[i] for i in indices_frente_direita]
-        self.obstaculo_a_frente_direita = self.distancias and min(self.distancias) < distancia_max_obstaculo_frente
+        distancias = [msg.ranges[i] for i in self.indices_frente_direita]
+        self.obstaculo_a_frente_direita = distancias and min(distancias) < distancia_max_obstaculo_frente
 
         self.obstaculo_a_frente = self.obstaculo_a_frente_direita or self.obstaculo_a_frente_esquerda
 
         # DETECÇÃO DE OBSTACULOS À ESQUERDA - Índices de +35° a +90°
         indices_esquerda = list(range(35, 90))
-        self.distancias = [msg.ranges[i] for i in indices_esquerda]
-        self.obstaculo_a_esquerda = self.distancias and min(self.distancias) < distancia_max_obstaculo_lados
+        distancias = [msg.ranges[i] for i in indices_esquerda]
+        self.obstaculo_a_esquerda = distancias and min(distancias) < distancia_max_obstaculo_lados
         # DETECÇÃO DE OBSTACULOS À DIREITA - Índices de -35° a -90°
         indices_direita = list(range(270, 325))
-        self.distancias = [msg.ranges[i] for i in indices_direita]
-        self.obstaculo_a_direita = self.distancias and min(self.distancias) < distancia_max_obstaculo_lados
+        distancias = [msg.ranges[i] for i in indices_direita]
+        self.obstaculo_a_direita = distancias and min(distancias) < distancia_max_obstaculo_lados
 
 
     def imu_callback(self, msg: Imu):
         return
         # Extraindo o quaternion da mensagem
-        orientation_q = msg.orientation
-        quat = [
-            orientation_q.x,
-            orientation_q.y,
-            orientation_q.z,
-            orientation_q.w
-        ]
-        # Conversão para Euler usando SciPy
-        r = R.from_quat(quat)
-        roll, pitch, yaw = r.as_euler('xyz', degrees=True)
-        # Exibindo resultados
-        self.get_logger().info('IMU Data Received:')
-        self.get_logger().info(
-            f'Orientation (Euler): Roll={roll:.2f}°, '
-            f'Pitch={pitch:.2f}°, Yaw={yaw:.2f}°'
-        )
-        self.get_logger().info(
-            f'Angular velocity: [{msg.angular_velocity.x:.2f}, '
-            f'{msg.angular_velocity.y:.2f}, {msg.angular_velocity.z:.2f}] rad/s'
-        )
-        self.get_logger().info(
-            f'Linear acceleration: [{msg.linear_acceleration.x:.2f}, '
-            f'{msg.linear_acceleration.y:.2f}, {msg.linear_acceleration.z:.2f}] m/s²'
-        )
+        # orientation_q = msg.orientation
+        # quat = [
+        #     orientation_q.x,
+        #     orientation_q.y,
+        #     orientation_q.z,
+        #     orientation_q.w
+        # ]
+        # # Conversão para Euler usando SciPy
+        # r = R.from_quat(quat)
+        # roll, pitch, yaw = r.as_euler('xyz', degrees=True)
+        # # Exibindo resultados
+        # self.get_logger().info('IMU Data Received:')
+        # self.get_logger().info(
+        #     f'Orientation (Euler): Roll={roll:.2f}°, '
+        #     f'Pitch={pitch:.2f}°, Yaw={yaw:.2f}°'
+        # )
+        # self.get_logger().info(
+        #     f'Angular velocity: [{msg.angular_velocity.x:.2f}, '
+        #     f'{msg.angular_velocity.y:.2f}, {msg.angular_velocity.z:.2f}] rad/s'
+        # )
+        # self.get_logger().info(
+        #     f'Linear acceleration: [{msg.linear_acceleration.x:.2f}, '
+        #     f'{msg.linear_acceleration.y:.2f}, {msg.linear_acceleration.z:.2f}] m/s²'
+        # )
 
     def odom_callback(self, msg: Odometry):
         return
@@ -125,6 +132,7 @@ class ControleRobo(Node):
         # self.get_logger().info(f"x: {msg.pose.pose.position.x}")
         # self.get_logger().info(f"y: {msg.pose.pose.position.y}")
         # self.get_logger().info(f"z: {msg.pose.pose.position.z}")
+
 
     def camera_callback(self, msg: Image):
         # Converte mensagem ROS para imagem OpenCV (BGR)
@@ -175,6 +183,13 @@ class ControleRobo(Node):
             self.bandeira_a_frente = False
             self.porcentagem_bandeira_na_camera = 0.0
 
+
+    def mover_garra(self, gripper_extension, right_gripper_joint, left_gripper_joint):
+            msg = Float64MultiArray()
+            msg.data = [gripper_extension, right_gripper_joint, left_gripper_joint]
+            self.gripper_pub.publish(msg)
+
+
     def move_robot(self):
         if self.estado_anterior != self.estado_atual:
             self.get_logger().info(f"## {self.estado_atual.name} ##")
@@ -197,19 +212,43 @@ class ControleRobo(Node):
                 # Caso contrario, continua andando para frente
                 else:
                     twist.linear.x = base_vel_linear
-            # Se tem obstaculo no caminho, desvia para o lado oposto
+                    if self.obstaculo_a_esquerda and not self.obstaculo_a_direita:
+                        # Se tiver algo à esquerda, gira levemente para a direita
+                        twist.angular.z = base_vel_angular * DirecoesRotacao.DIREITA.value * 0.5 
+                    elif self.obstaculo_a_direita and not self.obstaculo_a_esquerda:
+                        # Se tiver algo à direita, gira levemente para a esquerda
+                        twist.angular.z = base_vel_angular * DirecoesRotacao.ESQUERDA.value * 0.5
+
+            # Se tem obstaculo no caminho, entra no modo de desvio
             else:
                 self.estado_atual = Estados.DESVIANDO_DE_OBSTACULO
 
         ## DESVIANDO DE OBSTACULO: o robô vira para o lado oposto do obstáculo e anda para frente
         elif self.estado_atual == Estados.DESVIANDO_DE_OBSTACULO:
             if self.obstaculo_a_frente:
-                self.direcao_desvio = 1 if self.obstaculo_a_frente_direita else -1
-                twist.angular.z = base_vel_angular * self.direcao_desvio * 0.75
+                obstaculo_lados = self.obstaculo_a_direita or self.obstaculo_a_esquerda
+                # Obstaculo apenas à frente direita
+                if self.obstaculo_a_frente_direita and not self.obstaculo_a_frente_esquerda and not obstaculo_lados:
+                    self.direcao_desvio = DirecoesRotacao.ESQUERDA
+                # Obstáculo apenas à frente esquerda
+                elif self.obstaculo_a_frente_esquerda and not self.obstaculo_a_frente_direita and not obstaculo_lados:
+                    self.direcao_desvio = DirecoesRotacao.DIREITA
+                # Obstáculo apenas nas 2 frentes:
+                elif not obstaculo_lados:
+                    # vira para lado aleatório
+                    self.direcao_desvio = random.choice([DirecoesRotacao.ESQUERDA, DirecoesRotacao.DIREITA])
+                # Obstáculo nas duas frentes E na direita:
+                elif self.obstaculo_a_direita:
+                    self.direcao_desvio = DirecoesRotacao.ESQUERDA
+                # Obstáculo nas duas frentes E na esquerda:
+                elif self.obstaculo_a_esquerda:
+                    self.direcao_desvio = DirecoesRotacao.DIREITA
+                
+                twist.angular.z = base_vel_angular * self.direcao_desvio.value * 0.75
                 self.tempo_desviando = 5  # depois de sair da frente do obstaculo, desvia por mais 5 loops
 
             elif self.tempo_desviando > 0:
-                twist.angular.z = base_vel_angular * self.direcao_desvio * 0.75
+                twist.angular.z = base_vel_angular * self.direcao_desvio.value * 0.75
                 twist.linear.x = base_vel_linear * 0.5
                 self.tempo_desviando -= 1
 
@@ -232,7 +271,7 @@ class ControleRobo(Node):
                     self.estado_atual = Estados.POSICIONANDO_PARA_COLETA
             # Alinha robo com a bandeira
             elif (direcao_bandeira > 0 and self.obstaculo_a_esquerda) or (direcao_bandeira < 0 and self.obstaculo_a_direita):
-                twist.linear.x = base_vel_linear * 0.50
+                twist.linear.x = base_vel_linear * 0.70
                 twist.angular.z = base_vel_angular * 0.20 * (direcao_bandeira if not bandeira_centralizada else 0)
             else:
                 twist.linear.x = base_vel_linear 
@@ -258,6 +297,13 @@ class ControleRobo(Node):
             else:
                 self.get_logger().info("Garra posicionada!")
                 self.estado_atual = Estados.CAPTURANDO_BANDEIRA
+
+        ## CAPTURANDO BANDEIRA
+        elif self.estado_atual == Estados.CAPTURANDO_BANDEIRA:
+            # Garante que o robô fique parado
+            twist.linear.x = 0.0
+            twist.angular.z = 0.0
+            self.mover_garra(0.36, -0.05, 0.05)
 
         self.cmd_vel_pub.publish(twist)
 
